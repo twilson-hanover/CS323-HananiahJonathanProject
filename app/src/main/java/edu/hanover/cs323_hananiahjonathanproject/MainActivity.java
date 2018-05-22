6d6bf0f6daa8b1d4f0dce862b0616c757db73add
Jonathan Helmers <helmersj20@hanover.edu>
2018-05-22 14:18:42 -0400

    Added button on main page and a toast

--- app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.hanover.cs323_hananiahjonathanproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, OrderConfirmation.class);
+        intent.putExtra(OrderConfirmation.EXTRA_MESSAGE,
+                getResources().getString(R.string.button_response));
+        startService(intent);
     }
 }
---
+++ app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/OrderConfirmation.java
@@ -0,0 +1,42 @@
+package edu.hanover.cs323_hananiahjonathanproject;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class OrderConfirmation extends IntentService {
+    public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
+
+    public OrderConfirmation() {
+        super("OrderConfirmation");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        synchronized (this) {
+            try {
+                wait(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+    private void showText(final String text) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
