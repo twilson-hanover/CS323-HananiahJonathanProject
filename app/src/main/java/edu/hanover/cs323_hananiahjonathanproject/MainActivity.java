eb492965086b310d4bc7c0d5db06051ed0943dfe
Jonathan Helmers <helmersj20@hanover.edu>
2018-05-23 11:39:36 -0400

    Added drink and food classes, and maybe fixed the listView???

--- app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
@@ -7,12 +7,34 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listView,
+                                            View v,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                    FoodActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Intent intent = new Intent(MainActivity.this,
+                                    DrinkActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
     public void onClick(View view) {
         Intent intent = new Intent(this, OrderConfirmation.class);
@@ -20,21 +42,5 @@ public class MainActivity extends Activity {
                 getResources().getString(R.string.button_response));
         startService(intent);
     }
-    //Create an OnItemClickListener
-    AdapterView.OnItemClickListener itemClickListener =
-            new AdapterView.OnItemClickListener(){
-                public void onItemClick(AdapterView<?> listView,
-                                        View v,
-                                        int position,
-                                        long id) {
-                    if (position == 0) {
-                        Intent intent = new Intent(MainActivity.this,
-                                OrderConfirmation.class);
-                        startActivity(intent);
-                    }
-                }
-            };
-    //Add the listener to the list view
-//    ListView listView = (ListView) findViewById(R.id.list_options);
-//    listView.setOnItemClickListener(itemClickListener);
+
 }
