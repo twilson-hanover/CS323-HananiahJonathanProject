c4474c989de7c9f2f1fc4da266fdd9b3708f12d7
Jonathan Helmers <helmersj20@hanover.edu>
2018-05-23 10:53:17 -0400

    Added the empty drink and food activities

--- app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_hananiahjonathanproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -18,4 +20,21 @@ public class MainActivity extends Activity {
                 getResources().getString(R.string.button_response));
         startService(intent);
     }
+    //Create an OnItemClickListener
+    AdapterView.OnItemClickListener itemClickListener =
+            new AdapterView.OnItemClickListener(){
+                public void onItemClick(AdapterView<?> listView,
+                                        View v,
+                                        int position,
+                                        long id) {
+                    if (position == 0) {
+                        Intent intent = new Intent(MainActivity.this,
+                                OrderConfirmation.class);
+                        startActivity(intent);
+                    }
+                }
+            };
+    //Add the listener to the list view
+//    ListView listView = (ListView) findViewById(R.id.list_options);
+//    listView.setOnItemClickListener(itemClickListener);
 }
